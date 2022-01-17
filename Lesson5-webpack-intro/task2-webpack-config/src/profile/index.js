9abc5792335e222ba6df5572f7b67cfd32036c69
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-01-18 01:00:27 +0200

    webpack config

---
+++ Lesson5-webpack-intro/task2-webpack-config/src/profile/index.js
@@ -0,0 +1,10 @@
+
+import { fetchUser } from './gateway';
+import printProfile from './printProfile';
+
+fetchUser('github').then(({ name, location }) =>
+  printProfile({
+    name,
+    location,
+  }),
+);
