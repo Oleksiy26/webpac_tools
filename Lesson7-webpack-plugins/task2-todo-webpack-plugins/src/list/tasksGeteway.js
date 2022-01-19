4e0322bf5b6099fbd034448f2cbb9d291c77d4a2
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-01-19 05:28:15 +0200

    todo-plugins

---
+++ Lesson7-webpack-plugins/task2-todo-webpack-plugins/src/list/tasksGeteway.js
@@ -0,0 +1,27 @@
+const baseUrl = 'https://61dcca24591c3a0017e1aa84.mockapi.io/api/v1/tasks';
+
+export const getTasksList = () => {
+  return fetch(baseUrl).then((response) => response.json());
+};
+
+export const createTask = (taskData) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify(taskData),
+  });
+};
+
+export const updateTask = (taskId, updatedTaskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify(updatedTaskData),
+  });
+};
+
+export const deleteTask = (taskId) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  });
+};
